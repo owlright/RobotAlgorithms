55377c44777fef1b0f966da082ee194dd904fa89
owlnight <jhsheng@mail.ustc.edu.cn>
2025-05-31 22:35:26 +0800

    添加ICP3d::Options

--- include/icp3d.h
+++ include/icp3d.h
@@ -1,17 +1,35 @@
 #pragma once
 #include "common.h"
-#include <numeric>  // for std::accumulate
+#include <numeric> // for std::accumulate
 namespace ra {
 
 class ICP3d {
 public:
-    ICP3d() : source_(nullptr), target_(nullptr), source_center_(Vec3d::Zero()), target_center_(Vec3d::Zero()) {}
+    struct Options {
+        int max_iteration_ = 20; // 最大迭代次数
+        double max_nn_distance_ = 1.0; // 点到点最近邻查找时阈值
+        double max_plane_distance_ = 0.05; // 平面最近邻查找时阈值
+        double max_line_distance_ = 0.5; // 点线最近邻查找时阈值
+        int min_effective_pts_ = 10; // 最近邻点数阈值
+        double eps_ = 1e-2; // 收敛判定条件
+        bool use_initial_translation_ = false; // 是否使用初始位姿中的平移估计
+    };
+
+public:
+    ICP3d()
+        : source_(nullptr)
+        , target_(nullptr)
+        , source_center_(Vec3d::Zero())
+        , target_center_(Vec3d::Zero())
+    {
+    }
     ~ICP3d() = default;
     void SetSource(CloudPtr source)
     {
         source_ = source;
         source_center_ = std::accumulate(source_->points.begin(), source_->points.end(), Vec3d::Zero().eval(),
-                             [](const Vec3d& c, const PointType& pt) -> Vec3d { return c + ToVec3d(pt); }) / source_->size();
+                             [](const Vec3d& c, const PointType& pt) -> Vec3d { return c + ToVec3d(pt); })
+            / source_->size();
         LOG(INFO) << "source center: " << source_center_.transpose();
     }
 
@@ -19,7 +37,8 @@ public:
     {
         target_ = target;
         target_center_ = std::accumulate(target_->points.begin(), target_->points.end(), Vec3d::Zero().eval(),
-                             [](const Vec3d& c, const PointType& pt) -> Vec3d { return c + ToVec3d(pt); }) / target_->size();
+                             [](const Vec3d& c, const PointType& pt) -> Vec3d { return c + ToVec3d(pt); })
+            / target_->size();
         LOG(INFO) << "target center: " << target_center_.transpose();
     }
     /// 使用gauss-newton方法进行配准, 点到点
@@ -30,6 +49,7 @@ public:
 
     /// 基于gauss-newton的点面ICP
     bool AlignP2Plane(SE3& init_pose);
+
 private:
     CloudPtr source_;
     CloudPtr target_;
